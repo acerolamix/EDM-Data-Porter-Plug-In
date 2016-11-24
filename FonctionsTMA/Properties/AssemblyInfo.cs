054d85bed04d867aaf52188fdb3869fa624ed50c
Stringer <chicodelloft@hotmail.com>
2016-11-24 10:58:57 +0100

    Suppression culture dans le manifeste des assembly Remise à niveau des versions

--- FonctionsTMA/Properties/AssemblyInfo.cs
+++ FonctionsTMA/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("FonctionsTMA")]
 [assembly: AssemblyCopyright("Copyright ©SCOR  2016")]
 [assembly: AssemblyTrademark("SCOR")]
-[assembly: AssemblyCulture("fr")]
+[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -34,5 +34,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: NeutralResourcesLanguage("fr")]
 
--- ToolBoxTMA/Properties/AssemblyInfo.cs
+++ ToolBoxTMA/Properties/AssemblyInfo.cs
@@ -10,8 +10,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCompany("SCOR")]
 [assembly: AssemblyProduct("ToolBoxTMA")]
 [assembly: AssemblyCopyright("Copyright ©SCOR  2016")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("fr")]
+[assembly: AssemblyTrademark("SCOR")]
+[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -32,4 +32,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
